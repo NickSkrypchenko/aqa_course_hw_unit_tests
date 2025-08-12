b38bc0334cd45a4367b2a2252028198b5868e5ba
Nick Skrypchenko <nick.skrypchenko@gmail.com>
2025-08-12 19:26:35 +0300

    solved hw-6

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,54 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 let resultUnique;
 let resultNull;
+
+let competitorPizzasLow = [];
+for(let i = 0; i < competitorPizzas.length; i++) {
+  competitorPizzasLow.push(competitorPizzas[i].toLowerCase());
+}
+
+let myPizzasT1Low = [];
+for(let i = 0; i < myPizzasT1.length; i++) {
+  myPizzasT1Low.push(myPizzasT1[i].toLowerCase());
+}
+
+let myPizzasT2Low = [];
+for(let i = 0; i < myPizzasT2.length; i++) {
+  myPizzasT2Low.push(myPizzasT2[i].toLowerCase());
+}
+
+
+let resultUniqueT1 = [];
+for (let i = 0; i < myPizzasT1.length; i++) {
+  if (!competitorPizzasLow.includes(myPizzasT1[i].toLowerCase())) {
+    resultUniqueT1.push(myPizzasT1[i]);
+  }
+}
+if (resultUniqueT1.length === 0) {
+  resultNull = null; 
+} else {
+  resultUnique = resultUniqueT1;
+}
+
+
+let resultUniqueT2 = [];
+for (let i = 0; i < myPizzasT2.length; i++) {
+  if (!competitorPizzasLow.includes(myPizzasT2[i].toLowerCase())) {
+    resultUniqueT2.push(myPizzasT2[i]);
+  }
+}
+
+if (resultUniqueT2.length === 0) {
+  resultNull = null; 
+} else {
+  resultUnique = resultUniqueT2;
+}
+console.log(resultUnique);
+console.log(resultNull);
 
 export { resultNull, resultUnique };
