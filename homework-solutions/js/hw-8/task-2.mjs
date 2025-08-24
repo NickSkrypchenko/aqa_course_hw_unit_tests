a51ab887e7357527ea437cfb0ccbb80594f12697
Nick Skrypchenko <nick.skrypchenko@gmail.com>
2025-08-24 12:08:41 +0300

    solved hw-8 (tasks 1,2)

--- homework-solutions/js/hw-8/task-2.mjs
+++ homework-solutions/js/hw-8/task-2.mjs
@@ -18,8 +18,18 @@ const words = [
   'queue',
 ];
 
+function countVowels(word) {
+  const vowels = "aeiou";
+  let count = 0;
+  for (let char of word.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
 function sortedByVowels(wordsArr) {
-  // Ваш код
+  return [...wordsArr].sort((a, b) => countVowels(a) - countVowels(b));
 }
 
 export { sortedByVowels };
